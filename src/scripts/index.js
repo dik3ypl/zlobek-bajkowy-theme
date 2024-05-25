11af505979f493d4c339548661b15bab0c7381e2
Michał Proć <michalproc4@gmail.com>
2024-05-25 22:28:35 +0200

    Work in Progress: update content

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,7 @@
+import $ from 'jquery';
+import 'slick-carousel';
+
+// Navbar
 document.addEventListener('DOMContentLoaded', () => {
     let hamburger = document.querySelector('.hamburger')
     let navbar = document.querySelector('.navbar')
@@ -5,4 +9,18 @@ document.addEventListener('DOMContentLoaded', () => {
         hamburger.classList.toggle("hamburger-active")
         navbar.classList.toggle('navbar-revealed')
     })
+});
+
+// Slider
+
+$(document).ready(function () {
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 300
+    });
 });
