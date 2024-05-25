ecb98f2ea5b3be63b4e8ed9dea68cc148f6869f3
Michał Proć <michalproc4@gmail.com>
2024-05-25 02:20:10 +0200

    slick-carousel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Strona została załadowana.');
+import $ from 'jquery'
+import 'slick-carousel'
+
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 300
+    });
 });
