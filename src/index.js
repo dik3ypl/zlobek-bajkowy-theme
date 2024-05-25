9e18c45e56bbdbe420e5d96573882c5d76188cd5
Jakub Wiechniak <wiechniak.j@gmail.com>
2024-05-25 02:51:46 +0200

    dropdown menu on narrow screen

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     let hamburger = document.querySelector('.hamburger')
+    let navbar = document.querySelector('.navbar')
     hamburger.addEventListener("click", () => {
         hamburger.classList.toggle("hamburger-active")
+        navbar.classList.toggle('navbar-revealed')
     })
 });
