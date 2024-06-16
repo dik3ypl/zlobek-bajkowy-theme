0320d5f3210572172074a539619c661259963ca9
Jakub Wiechniak <wiechniak.j@gmail.com>
2024-06-16 02:50:30 +0200

    footer

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
-
+console.log(path.resolve(__dirname, 'assets/js'))
 module.exports = {
     entry: './src/scripts/index.js',
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'assets/js')
     },
     mode: 'development'
 };
